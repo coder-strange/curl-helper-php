61c6226d31e704c2bdf6ef6c2683c61fc072b560
TheTom <taufeek6@gmail.com>
2017-11-13 12:18:24 +0530

    Create PHPCurl.php

---
+++ PHPCurl.php
@@ -0,0 +1,216 @@
+<?php
+class PHPCurl {
+    private $ch = null;
+    private $headers = array();
+    private $enableResponseHeaders = FALSE;
+    private $enableCurlDebug=FALSE;
+    private $errorCodes=array();
+    const HTTP_REQUEST_POST = "POST";
+    const HTTP_REQUEST_GET = "GET";
+    const HTTP_REQUEST_PUT = "PUT";
+    const HTTP_REQUEST_DELETE = "DELETE";
+    
+
+    public function __construct() {
+		$this->errorCodes=  $this->errorCodes();
+    }
+    public function execute($request_type = 'GET', $url = '', $data = array(), $encode_json = 0) {
+		
+		$this->init();
+		
+		curl_setopt($this->ch, CURLOPT_URL, $url);
+		curl_setopt($this->ch, CURLOPT_HEADER, $this->enableResponseHeaders);
+		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->ch, CURLOPT_VERBOSE, $this->enableCurlDebug);
+		
+		if ($encode_json && !empty($data)) {
+			$data = json_encode($data);
+			$this->headers[] = 'Content-Type: application/json';
+			$this->headers[] = 'Content-Length: ' . strlen($data);
+		} 
+		elseif($request_type==self::HTTP_REQUEST_GET) {
+			$data = http_build_query($data, '', '&');
+			$url.=(strstr('?',$url) ? '&': '?').$url;
+		}
+
+		if (!empty($this->headers)) {
+			curl_setopt($this->ch, CURLOPT_HTTPHEADER, $this->headers);
+		}
+
+		switch ($request_type) {
+
+			case PHPCurl::HTTP_REQUEST_POST:
+					curl_setopt($this->ch, CURLOPT_POST, true);
+			break;
+
+			case PHPCurl::HTTP_REQUEST_PUT:
+			case PHPCurl::HTTP_REQUEST_DELETE:
+			case PHPCurl::HTTP_REQUEST_GET:
+				curl_setopt($this->ch, CURLOPT_POST, false);
+				curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, $request_type);
+			break;
+
+		}
+		if($request_type!=self::HTTP_REQUEST_GET){
+				curl_setopt($this->ch, CURLOPT_POSTFIELDS, $data);
+		}
+		$response = new stdClass();
+		$response->bodyRaw = curl_exec($this->ch);
+		$response->body=$response->bodyRaw;
+		$is_json_data=@json_decode($response->bodyRaw);
+		if($is_json_data){
+			$response->body=  $is_json_data;
+		}
+		$response->error=@curl_errno($this->ch);
+		if($response->error){
+			$response->errorCode = @$this->errorCodes[$response->error];
+			$response->errorDescription= @curl_error($this->ch);
+		}
+		$response->httpCode=curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+		$response->contentType=curl_getinfo($this->ch,CURLINFO_CONTENT_TYPE);
+		
+		curl_close($this->ch);
+		$this->clear();
+		return $response;
+	}
+		
+	public function setHeader($header = '') {
+		if (!empty($header)) {
+			$this->headers[] = $header;
+		}
+	}
+
+    public function setHeaders($headers = array()) {
+		if (!empty($headers)) {
+			$this->headers = $headers;
+		}
+    }
+
+    public function SSLVerfiy($boolean = 0) {
+		$this->init();
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, $boolean);
+    }
+
+    public function followLocation($boolean = 0) {
+		$this->init();
+		curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, $boolean);
+    }
+    
+    public function ipResolveV4(){
+		$this->init();
+		curl_setopt($this->ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+	}
+
+    public function enableResponseHeaders($boolean = 0) {
+		$this->enableResponseHeaders = $boolean;
+	}
+	
+    public function enableCurlDebug($boolean = 0) {
+		$this->enableCurlDebug = $boolean;
+    }
+
+    public function setBasicAuthentication($username, $password) {
+		$this->init();
+		curl_setopt($this->ch, CURLOPT_USERPWD, $username . ":" . $password);
+	}
+	
+    private function init(){
+		if($this->ch==null){
+			$this->ch = curl_init();
+		}
+	}
+	
+    private function clear() {
+		$this->baseurl = '';
+		$this->parameters = array();
+		$this->enableResponseHeaders = FALSE;
+		$this->headers = array();
+		$this->parameters = array();
+		@curl_close($this->ch);
+		$this->ch = null;
+    }
+
+    private function errorCodes() {
+			$error_codes = array(
+			1 => 'CURLE_UNSUPPORTED_PROTOCOL',
+			2 => 'CURLE_FAILED_INIT',
+			3 => 'CURLE_URL_MALFORMAT',
+			4 => 'CURLE_URL_MALFORMAT_USER',
+			5 => 'CURLE_COULDNT_RESOLVE_PROXY',
+			6 => 'CURLE_COULDNT_RESOLVE_HOST',
+			7 => 'CURLE_COULDNT_CONNECT',
+			8 => 'CURLE_FTP_WEIRD_SERVER_REPLY',
+			9 => 'CURLE_REMOTE_ACCESS_DENIED',
+			11 => 'CURLE_FTP_WEIRD_PASS_REPLY',
+			13 => 'CURLE_FTP_WEIRD_PASV_REPLY',
+			14 => 'CURLE_FTP_WEIRD_227_FORMAT',
+			15 => 'CURLE_FTP_CANT_GET_HOST',
+			17 => 'CURLE_FTP_COULDNT_SET_TYPE',
+			18 => 'CURLE_PARTIAL_FILE',
+			19 => 'CURLE_FTP_COULDNT_RETR_FILE',
+			21 => 'CURLE_QUOTE_ERROR',
+			22 => 'CURLE_HTTP_RETURNED_ERROR',
+			23 => 'CURLE_WRITE_ERROR',
+			25 => 'CURLE_UPLOAD_FAILED',
+			26 => 'CURLE_READ_ERROR',
+			27 => 'CURLE_OUT_OF_MEMORY',
+			28 => 'CURLE_OPERATION_TIMEDOUT',
+			30 => 'CURLE_FTP_PORT_FAILED',
+			31 => 'CURLE_FTP_COULDNT_USE_REST',
+			33 => 'CURLE_RANGE_ERROR',
+			34 => 'CURLE_HTTP_POST_ERROR',
+			35 => 'CURLE_SSL_CONNECT_ERROR',
+			36 => 'CURLE_BAD_DOWNLOAD_RESUME',
+			37 => 'CURLE_FILE_COULDNT_READ_FILE',
+			38 => 'CURLE_LDAP_CANNOT_BIND',
+			39 => 'CURLE_LDAP_SEARCH_FAILED',
+			41 => 'CURLE_FUNCTION_NOT_FOUND',
+			42 => 'CURLE_ABORTED_BY_CALLBACK',
+			43 => 'CURLE_BAD_FUNCTION_ARGUMENT',
+			45 => 'CURLE_INTERFACE_FAILED',
+			47 => 'CURLE_TOO_MANY_REDIRECTS',
+			48 => 'CURLE_UNKNOWN_TELNET_OPTION',
+			49 => 'CURLE_TELNET_OPTION_SYNTAX',
+			51 => 'CURLE_PEER_FAILED_VERIFICATION',
+			52 => 'CURLE_GOT_NOTHING',
+			53 => 'CURLE_SSL_ENGINE_NOTFOUND',
+			54 => 'CURLE_SSL_ENGINE_SETFAILED',
+			55 => 'CURLE_SEND_ERROR',
+			56 => 'CURLE_RECV_ERROR',
+			58 => 'CURLE_SSL_CERTPROBLEM',
+			59 => 'CURLE_SSL_CIPHER',
+			60 => 'CURLE_SSL_CACERT',
+			61 => 'CURLE_BAD_CONTENT_ENCODING',
+			62 => 'CURLE_LDAP_INVALID_URL',
+			63 => 'CURLE_FILESIZE_EXCEEDED',
+			64 => 'CURLE_USE_SSL_FAILED',
+			65 => 'CURLE_SEND_FAIL_REWIND',
+			66 => 'CURLE_SSL_ENGINE_INITFAILED',
+			67 => 'CURLE_LOGIN_DENIED',
+			68 => 'CURLE_TFTP_NOTFOUND',
+			69 => 'CURLE_TFTP_PERM',
+			70 => 'CURLE_REMOTE_DISK_FULL',
+			71 => 'CURLE_TFTP_ILLEGAL',
+			72 => 'CURLE_TFTP_UNKNOWNID',
+			73 => 'CURLE_REMOTE_FILE_EXISTS',
+			74 => 'CURLE_TFTP_NOSUCHUSER',
+			75 => 'CURLE_CONV_FAILED',
+			76 => 'CURLE_CONV_REQD',
+			77 => 'CURLE_SSL_CACERT_BADFILE',
+			78 => 'CURLE_REMOTE_FILE_NOT_FOUND',
+			79 => 'CURLE_SSH',
+			80 => 'CURLE_SSL_SHUTDOWN_FAILED',
+			81 => 'CURLE_AGAIN',
+			82 => 'CURLE_SSL_CRL_BADFILE',
+			83 => 'CURLE_SSL_ISSUER_ERROR',
+			84 => 'CURLE_FTP_PRET_FAILED',
+			84 => 'CURLE_FTP_PRET_FAILED',
+			85 => 'CURLE_RTSP_CSEQ_ERROR',
+			86 => 'CURLE_RTSP_SESSION_ERROR',
+			87 => 'CURLE_FTP_BAD_FILE_LIST',
+			88 => 'CURLE_CHUNK_FAILED'
+		);
+		$this->errorCodes=$error_codes;
+		return $this->errorCodes;
+    }
+}
